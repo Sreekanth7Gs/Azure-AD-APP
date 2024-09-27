34cfd2fea28d26d01ee418dd6b4b91859ed835ce
Sreekanth7Gs <96763290+Sreekanth7Gs@users.noreply.github.com>
2024-09-27 14:37:37 +0530

    Delete azure_active_directory_api_keys.py

--- azure_active_directory_api_keys.py
+++
@@ -1,8 +0,0 @@
-- text: |
-    env:
-      AZURE_AD_CLIENT_ID: "a6c118a8-63d5-47ec-b21e-472d216ca305"
-      AZURE_AD_CLIENT_SECRET: "CUi8Q~_hdh8i50RHTAyDHlRAWm0OKehz-TX60cY~"
-      AZURE_AD_TENANT_ID: "38fb3075-dca4-499e-8123-bbc3ab1e2ef2"
-  client_id: a6c118a8-63d5-47ec-b21e-472d216ca305
-  tenant_id: 38fb3075-dca4-499e-8123-bbc3ab1e2ef2
-  client_secret: CUi8Q~_hdh8i50RHTAyDHlRAWm0OKehz-TX60cY~
